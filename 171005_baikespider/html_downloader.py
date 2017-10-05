2934e511701a60fa5ecefe866e9cfbd0b670a1a3
shinespeciall <648616375@qq.com>
2017-10-05 15:46:16 +0800

    Update

---
+++ 171005_baikespider/html_downloader.py
@@ -0,0 +1,15 @@
+import urllib2
+class HtmlDownloader(object):
+    def __init__(self):
+        pass
+
+    def download(self, url):
+        if url is None:
+            return None
+
+        response = urllib2.urlopen(url)
+
+        if response.getcode() != 200:
+            return
+        else:
+            return response.read()
